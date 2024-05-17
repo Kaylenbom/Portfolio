7bf80d7ef050dd048df44dfa4c2bd37d748bc40e
kaylenbom <kcb326@drexel.edu>
2024-05-17 13:52:18 -0400

    about me section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,43 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
+/*===== SHOW MENU =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== ADD BLUR TO HEADER ===============*/
-
+const blurHeader = () =>{
+    const heaader = document.getElementById('header')
+    //When the scroll is greater than 50 viewport height, add the blur-header class to the heaader tag
+    this.scrollY >= 50 ? Headers.classList.add('blur-header')
+                       : Headers.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 /*=============== EMAIL JS ===============*/
 
